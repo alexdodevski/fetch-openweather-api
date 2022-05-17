89c64334a3b8ebaff8547e7933099f332a692341
Alexander Dodevski <alexanderdodevski@gmail.com>
2022-05-17 20:21:26 +0300

    commit for gh-pages

--- modules/Request.js
+++ modules/Request.js
@@ -8,7 +8,7 @@ export default class Request {
   async getDataOpenMap() {
     return getCoords().then((coords) => {
       return new Promise((resolve) =>
-        // задержка для подгрузки координатов из getCoords , т.к если получить их ДО таймера будет undefined
+        // задержка для подгрузки координатов из getCoords ,т.к если получить их ДО таймера будет undefined
         setTimeout(() => {
           const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=f366574461000687067bb791f7dbd2d4`;
           resolve(this._reguest(url));
