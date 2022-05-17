e783cf413b52141b4f83e1b61f9ee66475eb942d
Alexander Dodevski <alexanderdodevski@gmail.com>
2022-05-17 20:13:52 +0300

    finish refactoring

--- modules/App.js
+++ modules/App.js
@@ -17,46 +17,54 @@ export default class App {
     this.app.append(waiting);
   }
 
+  _animationElems() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        Array.from(this.app.children).forEach((item) => {
+          item.style.opacity = 1;
+        });
+      }, 0);
+      resolve();
+    });
+  }
+
+  _selectEvent() {
+    const select = this.childs[2];
+    select.addEventListener("change", () => {
+      let elems = [];
+      for (let child of this.childs) {
+        if (child.tagName === "DIV") {
+          elems.push(child);
+        }
+      }
+      this.changeLanguage(elems, select.children);
+    });
+  }
+
+  _delayRender() {
+    return new Promise((res) => {
+      // задержка для полной подгрузки страницы, пока страница не подгружена, то работает прелоадер
+      setTimeout(() => {
+        this.app.children[0].remove();
+        this.childs.forEach((item) => {
+          this.app.append(item);
+        });
+        res();
+      }, 500);
+    });
+  }
+
   render(elems) {
-    const promise = new Promise((resolve) => {
+    const promiseRender = new Promise((resolve) => {
       for (let elem of elems) {
         this.childs.push(new elem().init());
       }
       resolve();
     });
 
-    promise
-      .then(() => {
-        return new Promise((res) => {
-          // задержка для полной подгрузки страницы, пока страница не подгружена, то работает прелоадер
-          setTimeout(() => {
-            this.app.children[0].remove();
-            this.childs.forEach((item) => {
-              this.app.append(item);
-            });
-            res();
-          }, 500);
-        });
-      })
-      .then(() => {
-        // задержка для анимации
-        setTimeout(() => {
-          Array.from(this.app.children).forEach((item) => {
-            item.style.opacity = 1;
-          });
-        }, 0);
-      })
-      .then(() => {
-        const select = this.childs[2];
-        select.addEventListener("change", () => {
-          let elems = [];
-          for (let child of this.childs) {
-            if (child.tagName === "DIV") {
-              elems.push(child);
-            }
-          }
-          this.changeLanguage(elems, select.children);
-        });
-      });
+    promiseRender
+      .then(() => this._delayRender())
+      .then(() => this._animationElems())
+      .then(() => this._selectEvent());
   }
 }
