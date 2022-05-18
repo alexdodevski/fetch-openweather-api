411453f183b6313e2b95229094d5d6aee0a06e69
Alexander Dodevski <alexanderdodevski@gmail.com>
2022-05-18 11:04:26 +0300

    add words for translations.js

--- utility/translations.js
+++ utility/translations.js
@@ -9,11 +9,13 @@ export const translateOnRus = {
     Clouds: "Облачно",
     Sun: "Солнечно",
     Rain: "Дождь",
+    Clear: "Ясно",
   },
   description: {
     "broken clouds": "небольшая облачность",
     "overcast clouds": "пасмурно",
     "light rain": "легкий дождь",
+    "clear sky": "ясное небо",
   },
 };
 
@@ -28,10 +30,12 @@ export const translateOnEng = {
     Облачно: "Clouds",
     Солнечно: "Sun",
     Дождь: "Rain",
+    Ясно: "Clear",
   },
   description: {
     "небольшая облачность": "broken clouds",
     пасмурно: "overcast clouds",
     "легкий дождь": "light rain",
+    "ясное небо": "clear sky",
   },
 };
