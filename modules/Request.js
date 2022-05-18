fab549c875a7e8eb1224caaa108d46383ae37ad5
Alexander Dodevski <alexanderdodevski@gmail.com>
2022-05-18 11:11:55 +0300

    change reques

--- modules/Request.js
+++ modules/Request.js
@@ -6,7 +6,6 @@ export default class Request {
   }
   async getDataOpenMap() {
     return new Promise((resolve) => {
-      // задержка для подгрузки координатов из getCoords ,т.к если получить их ДО таймера будет undefined
       let url;
       navigator.geolocation.getCurrentPosition(({ coords }) => {
         let { latitude, longitude } = coords;
@@ -14,9 +13,8 @@ export default class Request {
         longitude = longitude.toFixed(2);
         url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f366574461000687067bb791f7dbd2d4`;
       });
-
+      // задержка для подгрузки координатов из getCoords ,т.к если получить их ДО таймера будет undefined
       setTimeout(() => {
-        console.log(url);
         resolve(this._reguest(url));
       }, 500);
     });
